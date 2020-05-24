0c155ef9c9863e454d1846e493dfd4a58d33cd0b
Luiz Augusto <luiz158@gmail.com>
2020-05-24 18:44:07 -0300

    with initial data in the h2 database...

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+
+insert into ciudades (id,ciudad) values (1,'Curitiba');
+insert into ciudades (id,ciudad) values (2,'Ponta Grossa');
+insert into ciudades (id,ciudad) values (3,'Japira');
+insert into ciudades (id,ciudad) values (4,'Rio Azul');
+
+
+insert into clientes (id,nombres,apellidos,telefono,email,ciudades_id) values (1,'Fulano 1','Apelido 1','9999-9999','fulano1@aqui.com.br',1);
+insert into clientes (id,nombres,apellidos,telefono,email,ciudades_id) values (2,'Fulano 2','Apelido 2','9999-9999','fulano2@aqui.com.br',2);
+insert into clientes (id,nombres,apellidos,telefono,email,ciudades_id) values (3,'Fulano 3','Apelido 3','9999-9999','fulano3@aqui.com.br',3);
+insert into clientes (id,nombres,apellidos,telefono,email,ciudades_id) values (4,'Fulano 4','Apelido 4','9999-9999','fulano4@aqui.com.br',4);
+insert into clientes (id,nombres,apellidos,telefono,email,ciudades_id) values (5,'Fulano 5','Apelido 5','9999-9999','fulano5@aqui.com.br',4);
+insert into clientes (id,nombres,apellidos,telefono,email,ciudades_id) values (6,'Fulano 6','Apelido 6','9999-9999','fulano6@aqui.com.br',3);
+
